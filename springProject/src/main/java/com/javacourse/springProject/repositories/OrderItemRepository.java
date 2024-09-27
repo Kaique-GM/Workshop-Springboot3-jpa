6f1bbe111aa1095dd9497c5e6879fe6ba2a4ad1a
Kaique-GM <kaiquegmoficial@gmail.com>
2024-09-27 13:58:45 -0300

    Product-OrderItem one-to-many association

--- springProject/src/main/java/com/javacourse/springProject/repositories/OrderItemRepository.java
+++ springProject/src/main/java/com/javacourse/springProject/repositories/OrderItemRepository.java
@@ -3,8 +3,9 @@ package com.javacourse.springProject.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.javacourse.springProject.entities.OrderItem;
+import com.javacourse.springProject.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem,OrderItemPK> {
 
 
 }
